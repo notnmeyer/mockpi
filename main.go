1194eb22b3edf115ff63a6c6cd442ef5ecdf83f6
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-10-19 12:01:31 -0700

    feat: tests and test ci workflow (#5)

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -9,28 +10,58 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		responseBody := r.Header["X-Response-Json"][0]
+		var (
+			contentType  = "application/json"
+			responseBody = r.Header["X-Response-Json"][0]
+			responseCode int
+		)
 
-		var responseCode int
-		if val, ok := r.Header["X-Response-Code"]; !ok {
-			responseCode = 200
-		} else {
-			var err error
-			responseCode, err = strconv.Atoi(val[0])
-			if err != nil {
-				fmt.Println("not a value status code: ", err)
-			}
+		responseCode, err := validateResponseCode(r.Header)
+		if err != nil {
+			responseBody = err.Error()
+			contentType = "text/plain"
+		}
+
+		if !isJSON(responseBody) {
+			responseBody = fmt.Errorf("x-response-json must be valid JSON").Error()
+			responseCode = http.StatusBadRequest
+			contentType = "text/plain"
 		}
 
 		w.WriteHeader(responseCode)
-		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Content-Type", contentType)
 		w.Write([]byte(responseBody))
 	})
 
 	listenAddr := ":8080"
-	fmt.Printf("Starting server on %s...", listenAddr)
+	fmt.Printf("Listening on %s...\n", listenAddr)
 	if err := http.ListenAndServe(listenAddr, nil); err != nil {
 		fmt.Println("server error: ", err)
 		os.Exit(1)
 	}
+}
+
+func validateResponseCode(header map[string][]string) (int, error) {
+	if val, exists := header["X-Response-Code"]; exists {
+		// verify its a number
+		code, err := strconv.Atoi(val[0])
+		if err != nil {
+			return http.StatusBadRequest, fmt.Errorf("x-response-code must be a number\n")
+		}
+
+		// verify it falls in the range of status codes
+		if !(code >= 100 && code <= 599) {
+			return http.StatusBadRequest, fmt.Errorf("x-response-code must be between 100 and 599\n")
+		}
+
+		return code, nil
+	}
+
+	// if x-response-code was not supplied default to a 200
+	return http.StatusOK, nil
+}
+
+func isJSON(s string) bool {
+	var j json.RawMessage
+	return json.Unmarshal([]byte(s), &j) == nil
 }
