b09eb7ff0dfb75dcde4228a93a545e17cf7b2cbb
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-10-21 19:10:51 -0700

    chore: handler tests (#9)

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/notnmeyer/mockpi
 
 go 1.20
+
+require github.com/spf13/pflag v1.0.5
--- main.go
+++ main.go
@@ -6,39 +6,51 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+
+	flag "github.com/spf13/pflag"
 )
 
+type config struct {
+	port int
+}
+
+var c config
+
+func init() {
+	flag.IntVarP(&c.port, "port", "p", 8080, "the listen port")
+	flag.Parse()
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		var (
-			contentType  = "application/json; charset=utf-8"
-			responseBody = r.Header["X-Response-Json"][0]
-			responseCode int
-		)
-
-		responseCode, err := validateResponseCode(r.Header)
-		if err != nil {
-			responseBody = err.Error()
-			contentType = "text/plain; charset=utf-8"
-		}
-
-		if !isJSON(responseBody) {
-			responseBody = fmt.Errorf("x-response-json must be valid JSON").Error()
-			responseCode = http.StatusBadRequest
-			contentType = "text/plain; charset=utf-8"
-		}
-
-		w.Header().Set("Content-Type", contentType)
-		w.WriteHeader(responseCode)
-		w.Write([]byte(responseBody))
-	})
-
-	listenAddr := ":8080"
+	http.HandleFunc("/", handler)
+	listenAddr := fmt.Sprintf(":%d", c.port)
 	fmt.Printf("Listening on %s...\n", listenAddr)
 	if err := http.ListenAndServe(listenAddr, nil); err != nil {
 		fmt.Println("server error: ", err)
 		os.Exit(1)
 	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	var (
+		contentType  = "application/json; charset=utf-8"
+		responseBody = r.Header["X-Response-Json"][0]
+		responseCode int
+	)
+
+	responseCode, err := validateResponseCode(r.Header)
+	if err != nil {
+		responseBody = err.Error()
+		contentType = "text/plain; charset=utf-8"
+	} else if !isJSON(responseBody) {
+		responseBody = fmt.Errorf("x-response-json must be valid JSON").Error()
+		responseCode = http.StatusBadRequest
+		contentType = "text/plain; charset=utf-8"
+	}
+
+	w.Header().Set("Content-Type", contentType)
+	w.WriteHeader(responseCode)
+	w.Write([]byte(responseBody))
 }
 
 func validateResponseCode(header map[string][]string) (int, error) {
